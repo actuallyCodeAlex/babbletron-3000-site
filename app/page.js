86599848806232723d31e7a451e8f76fad621221
actuallyCodeAlex <133512594+actuallyCodeAlex@users.noreply.github.com>
2023-05-19 16:26:48 -0700

    Change font
    Change from "Inter" to "Caveat"

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1>babbeltron-3000</h1>
+        <h1>babbletron-3000</h1>
         <h2>June 1, 2023</h2>
       </div>
     </main>
