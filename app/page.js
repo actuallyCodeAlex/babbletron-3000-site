89386c9175d863fff74cabb7a10ff328835f16fb
actuallyCodeAlex <actuallyCodeAlex@gmail.com>
2023-06-11 08:24:36 -0700

    Change font sizes

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <h1>babbletron-3000</h1>
-        <h2>June 1, 2023</h2>
+        <h1 id='title'>babbletron-3000</h1>
+        <h2 id='date'>June 1, 2023</h2>
       </div>
     </main>
   )
